ac434bcdb26dc21d954e0a2de8747f6451786ddc
Methodician <methodician@gmail.com>
2019-06-18 10:09:48 -0700

    temp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { PrListComponent } from "./pr-list/pr-list.component";
 import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+// Angular Material
+import { MatButtonModule, MatTableModule } from "@angular/material";
 
 @NgModule({
   declarations: [AppComponent, PrListComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
